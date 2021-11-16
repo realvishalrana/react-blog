a9af5d7824fe55121518b6ce4ac2f6043a5d1056
vishal <vrana14693@gmail.com>
2021-11-16 12:19:33 +0530

    write page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      <Single />
+      <Write />
     </div>
   );
 }
---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,37 @@
+import "./write.css";
+
+const Write = () => {
+  return (
+    <div className="write">
+      <img
+        className="writeImg"
+        src="https://images.unsplash.com/photo-1555511920-5d0d9834f549?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
+        alt=""
+      />
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className="writeIcon fas fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </div>
+  );
+};
+
+export default Write;
