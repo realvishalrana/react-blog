f24512ca94ba71285ec898dcbdc652a18952ad52
vishal <vrana14693@gmail.com>
2021-11-16 15:56:07 +0530

    settings components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-
+import Settings from "./pages/settings/Settings";
 function App() {
   return (
     <div className="App">
       <TopBar />
-      <Write />
+      <Settings />
     </div>
   );
 }
